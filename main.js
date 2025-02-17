c541d02c35d54d9af51f7e0c1e8e53eecf2e5d57
ylro-gabriel <ylrgbrlmllr@gmail.com>
2025-02-17 14:15:15 +0800

    Create a function that adds one point per round to the winner

--- main.js
+++ main.js
@@ -13,20 +13,54 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+
 
 // make a function getHumanChoice that will 
 // prompt user to input a case senstive choice
 
 function getHumanChoice(){
     x = prompt("Choose: rock, paper, scissors?");
-    return x;
+    return x.toLowerCase();
 }
 
-console.log(getHumanChoice()); 
+
 
 // make scores for each functions and set their
 // values to 0
 
 let computerScore = 0;
 let humanScore = 0;
+
+//make a playRound function with the human and
+//computer choices as parameters
+// add a point to the winner of the round
+
+function playRound(human, computer){
+    human = getHumanChoice();
+    computer = getComputerChoice();
+
+    if(human == "rock" && computer == "paper"){
+        return computerScore++;
+    } else if(human == "rock" && computer == "scissors"){
+        return humanScore++;
+    } else if(human == "paper" && computer == "rock"){
+        return humanScore++;
+    } else if(human == "paper" && computer == "scissors"){
+        return computerScore++;
+    } else if(human == "scissors" && computer == "rock"){
+        return computerScore++;
+    } else if(human == "scissors" && computer == "paper"){
+        return humanScore++;
+    }   else{
+        return "Draw";
+    } 
+}
+
+// make a function that cllas playRound 5 times
+//
+
+function playGame(){
+    
+}
+
+playGame();
