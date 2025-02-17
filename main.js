297d05e1dab5b5a7adf196fa653b526cb3966aca
ylro-gabriel <ylrgbrlmllr@gmail.com>
2025-02-17 13:26:28 +0800

    Make computer opponent choice randomly

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hi")
+// make a function getCompiterChoice that will
+//randomly return "rock", "paper" or "scissors"
+
+function getComputerChoice(){
+    x = Math.random();
+
+    if(x <= 0.3){
+        return "rock";
+    } else if (x <= 0.6){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
