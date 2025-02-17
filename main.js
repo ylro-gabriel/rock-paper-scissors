ec57d4113ae8e9c6cb237c3838d556f20afd6758
ylro-gabriel <ylrgbrlmllr@gmail.com>
2025-02-17 13:36:55 +0800

    Change the code of getComputerChoice function

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 //randomly return "rock", "paper" or "scissors"
 
 function getComputerChoice(){
-    x = Math.random();
+    x = Math.random() * (3 - 0) + 0;
 
-    if(x <= 0.3){
+    if(x <= 1){
         return "rock";
-    } else if (x <= 0.6){
+    } else if (x <= 2){
         return "paper";
     } else {
         return "scissors";
@@ -14,3 +14,4 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice());
+
